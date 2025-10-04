4ba57b95c96c7daebff8d3e12f53f6c998e39991
Sébastien Garcia-Romeu <webmaster@nicewebagence.com>
2025-10-04 11:17:50 +0200

    change removeCircularJSONstringify type to any to reflect JSON.stringify type

--- src/remove-circular-json-stringify.ts
+++ src/remove-circular-json-stringify.ts
@@ -1,7 +1,7 @@
 
 
 /** Best way to stringify a value! Default indent: 2 */
-export function removeCircularJSONstringify(object: Record<string, any>, indent = 2) {
+export function removeCircularJSONstringify(object: any, indent = 2) {
     const getCircularReplacer = () => {
         const seen = new WeakSet()
         return (_: any, value: any) => {
